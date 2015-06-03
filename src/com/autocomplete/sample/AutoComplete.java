7f6a6871ab49f65be27b12adde2a16ea581c2427
abesseng <aboubacar.bessengadiallo@isep.fr>
2015-06-03 11:07:43 +0200

    modif autocomplete

--- src/com/autocomplete/sample/AutoComplete.java
+++ src/com/autocomplete/sample/AutoComplete.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
+import java.util.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 
 import com.DataSource.mysql.DataSource;
 
@@ -30,27 +31,79 @@ public class AutoComplete extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		ArrayList<String> al = new ArrayList<String>();
+
+		Hashtable<String, Object> AllResults = new Hashtable<String, Object>();
+		Hashtable<String, String> AuthorsResults = new Hashtable<String, String>();
+		Hashtable<String, String> TeamsResults = new Hashtable<String, String>();
+		Hashtable<String, String> PublicationsResults = new Hashtable<String, String>();
+
 		try {
 			DataSource ds = DataSource.getInstace();
 			Connection conn = ds.getConnection();
 			Statement stmt = conn.createStatement();
-			String sql = "SELECT firstname FROM authors " + "UNION "
-					+ "SELECT lastname FROM authors " + "UNION "
-					+ "SELECT title FROM publications " + "UNION "
-					+ "SELECT teamName FROM teams " + "UNION "
-					+ "SELECT typeName FROM types " + "ORDER BY 1";
-			ResultSet rs = stmt.executeQuery(sql);
-			while (rs.next()) {
-				al.add(rs.getString("firstname"));
+
+			// Requêtes
+			String sqlAuthors = "SELECT firstname,lastname FROM authors ";
+			String sqlPublications = "SELECT title,publicationId FROM publications ";
+			String sqlTeams = "SELECT title,publicationId FROM publications ";
+
+			// Résultats des requêtes
+			ResultSet rsAuthors = stmt.executeQuery(sqlAuthors);
+			ResultSet rsTeams = stmt.executeQuery(sqlTeams);
+			ResultSet rsPublications = stmt.executeQuery(sqlPublications);
+
+			// Stocage des données dans la Hashmap Author
+			while (rsAuthors.next()) {
+				String firstname = rsAuthors.getString("firstname");
+				String lastname = rsAuthors.getString("lastname");
+
+				AuthorsResults.put("Firstname", firstname);
+				AuthorsResults.put("Lastname", lastname);
 			}
-			rs.close();
+
+			// Stocage des données dans la Hashmap Team
+			while (rsTeams.next()) {
+				String teamId = rsTeams.getString("teamId");
+				String teamName = rsTeams.getString("teamName");
+
+				TeamsResults.put("TeamId", teamId);
+				TeamsResults.put("TeamName", teamName);
+			}
+
+			// Stocage des données dans la Hashmap Pub
+			while (rsPublications.next()) {
+				String publicationId = rsPublications
+						.getString("publicationId");
+				String title = rsPublications.getString("title");
+
+				PublicationsResults.put("PublicationId", publicationId);
+				PublicationsResults.put("Title", title);
+			}
+
+			// HashMap contenant toutes les données
+			AllResults.put("Publications", PublicationsResults);
+			AllResults.put("Teams", TeamsResults);
+			AllResults.put("Utilisateurs", AuthorsResults);
+
+			// Close
+			rsAuthors.close();
+			rsTeams.close();
+			rsPublications.close();
+
 			stmt.close();
 			conn.close();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		JSONArray json = new JSONArray(al);
+
+		JSONArray json = null;
+		try {
+			json = new JSONArray(AllResults);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		response.setContentType("application/json");
 		response.getWriter().print(json);
 	}
