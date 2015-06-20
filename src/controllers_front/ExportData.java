da473027ec6f149b66fcb167ad9446fe997321d0
abesseng <aboubacar.bessengadiallo@isep.fr>
2015-06-21 01:26:02 +0200

    probleme avec servlet.jar

--- src/controllers_front/ExportData.java
+++ src/controllers_front/ExportData.java
@@ -14,9 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.tomcat.jni.File;
-import org.omg.CORBA.DataInputStream;
-
 
 
 
